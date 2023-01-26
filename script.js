9f6258d61420c6c16b3fce0021aed2c6a74daba8
Aamer577 <123670890+Aamer577@users.noreply.github.com>
2023-01-26 11:35:29 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+let button1=document.querySelector(".story-opening");
+
+
+
+let button2=document.querySelector(".option-one");
+let button3=document.querySelector(".option-two");
+
+
+let screenoption1=document.querySelector(".option-one-screen");
+let screenoption2=document.querySelector(".option-two-screen");
+let screenoption3=document.querySelector("option-one-end");
+
+button1.onclick=function(){
+    screenoption1.style.display="non";
+
+
+};
+button2.onclick=function(){
+    screenoption2.style.display="block";
+
+
+};
+
+
+
+/*
+INSERT_VARIABLE.onclick=function(){
+
+
+
+button.onclick=function(){
+
+}
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
